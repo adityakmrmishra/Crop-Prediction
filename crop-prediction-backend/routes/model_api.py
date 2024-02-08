5b38b77eb7e00d844d38afa607ddb5e1c60c00c8
Adityamishra9719 <adityamishra971916@gmail.com>
2024-02-08 19:02:39 +0530

    crop Prediction

--- crop-prediction-backend/routes/model_api.py
+++ crop-prediction-backend/routes/model_api.py
@@ -8,9 +8,8 @@ import pickle
 
 
 # importing models
-model = pickle.load(open('D:\OPEN SOURCE\Crop Recommendation\crop-prediction-backend\models\model.pkl', 'rb'))
-sc = pickle.load(open('D:\OPEN SOURCE\Crop Recommendation\crop-prediction-backend\models\standscaler.pkl', 'rb'))
-ms = pickle.load(open('D:\OPEN SOURCE\Crop Recommendation\crop-prediction-backend\models\minmaxscaler.pkl', 'rb'))
+model = pickle.load(open('D:\OPEN SOURCE\Crop Recommendation\crop-prediction-backend\models\RandomForest.pkl', 'rb'))
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}}, allow_headers=["Content-Type"])  # Enable CORS for all routes
@@ -36,9 +35,9 @@ def predict():
         feature_list = [N, P, K, temp, humidity, ph, rainfall]
         single_pred = np.array(feature_list).reshape(1, -1)
         # Use the trained model to make predictions
-        scaled_features = ms.transform(single_pred)
-        final_features = sc.transform(scaled_features)
-        prediction = model.predict(final_features)
+        # scaled_features = ms.transform(single_pred)
+        # final_features = sc.transform(scaled_features)
+        prediction = model.predict(single_pred)
 
         crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
                     8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
